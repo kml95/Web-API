436a1d08b66526fb765705b382ecd55f5b48c7b6
kam95oleksy <kam95oleksy@gmail.com>
2018-03-11 11:07:36 +0100

    Add project files.

---
+++ WebApi/Api/AuthorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Api
+{
+    public class AuthorController : ApiController
+    {
+        protected AuthorController()
+        {
+        }
+
+        [HttpGet, Route("Api/GetAllAuthors")]
+        public List<Author> GetAll()
+        {
+            kokoEntities dbcontext = new kokoEntities();
+
+            return dbcontext.Author.ToList();
+        }
+
+        [HttpPost, Route("Api/AddAutho")]
+        public void AddAuthor(Author newAuthor)
+        {
+            kokoEntities dbcontext = new kokoEntities();
+
+            dbcontext.Author.Add(newAuthor);
+            dbcontext.SaveChanges();
+
+        }
+
+    }
+}
---
+++ WebApi/Scripts/controllers/authorCtrl.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('myAuthor', []);
+app.controller('myAuthorCtrl', function ($scope, $http) {
+  
+    $scope.GetAllAuthors = function () {
+        $http.get("/Api/GetAllAuthors")
+            .then(function (response) {
+                $scope.authors = response.data;
+            });
+    }
+
+    $scope.GetAllAuthors();
+});
